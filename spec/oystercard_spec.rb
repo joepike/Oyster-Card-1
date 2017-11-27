c6412b2e1d20f2d2cf1b263f5574b318e71d5715
joepike <joseph.l.pike@gmail.com>
2017-11-27 15:59:18 +0000

    Week2: Challenge 4

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,4 +2,9 @@ require 'oystercard'
 
 describe Oystercard do
 
+  it { is_expected.to respond_to :balance }
+
+  it 'should initialize with a balance of zero' do
+    expect(subject.balance).to eq 0
+  end
 end
