f53ee4f8f5599a408eff28df47781150d4c4f3be
Lan Pham <lanpham312@gmail.com>
2017-11-27 16:10:23 +0000

    Challenge 5 - Top-up

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -7,4 +7,19 @@ describe Oystercard do
   it 'should initialize with a balance of zero' do
     expect(subject.balance).to eq 0
   end
+
+  it "should let me top up with 10" do
+    expect(subject.top_up(10)).to eq 10
+  end
+
+  it "should let me top up with 15" do
+    expect(subject.top_up(15)).to eq 15
+  end
+
+  it "should add the top up amount to the balance" do
+    subject.top_up(10)
+    expect(subject.balance).to eq 10
+  end
+
+
 end
